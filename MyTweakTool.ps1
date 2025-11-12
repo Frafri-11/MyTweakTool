2ec6f0d683a3cb2882fa98973dcc5230a57c8b94
Frafri-11 <franciscosampino@gmail.com>
2025-11-12 02:01:53 +0100

    Update MyTweakTool.ps1

--- MyTweakTool.ps1
+++ MyTweakTool.ps1
@@ -4,25 +4,28 @@ Clear-Host
 # Imposta il titolo della finestra
 $Host.UI.RawUI.WindowTitle = "GPUWORLD Optimization Tool"
 
+# Avvio silenzioso
+Start-Sleep -Seconds 1.2
+Clear-Host
+
 @"
-GGGGGGGGGGGGG       WW               WW     TTTTTTTTTTTTTTTTTTT
-GG::::::::::::G      WW             WW      T:::::::::::::::::T
-GG:::::::::::::::G    WW     WW     WW      T:::::::::::::::::T
-G:::::GGGGGGGG::::G    WW   WWWW   WW       T:::::TT:::::::TTT
-G:::::G       GGGGGG    WW WWWWWW WW        TTTTTT  TTTTTTT
-G:::::G                 WWWW   WWWW                 T:::::T
-G:::::G    GGGGGGG      WW     WW                   T:::::T
-G:::::G    G:::::G      WW     WW                   T:::::T
-G:::::G    G:::::G      WW     WW                   T:::::T
-G:::::G    G:::::G      WW     WW                   T:::::T
-G:::::G       GGGGGG    WW     WW                   T:::::T
-G:::::GGGGGGGG::::G     WW     WW                   T:::::T
-GG:::::::::::::::G      WW     WW                   T:::::T
-GGG::::::GGG:::G        WW     WW                   T:::::T
-   GGGGGG   GGGG        WW     WW                   TTTTTTT
+GGGGGGGGGGGGGGG           WW                 WW       TTTTTTTTTTTTTTTTTTT
+GG:::::::::::::::G         WW               WW        T:::::::::::::::::T
+GG:::::::::::::::G          WW     WW     WW         T:::::::::::::::::T
+G:::::GGGGGGGG::::G          WW   WWWW   WW          T:::::TT:::::::TTT
+G:::::G         GGGGGG        WW WWWWWW WW           TTTTTT  TTTTTTT
+G:::::G                      WWWW     WWWW                   T:::::T
+G:::::G     GGGGGGG          WW         WW                   T:::::T
+G:::::G     G:::::G          WW         WW                   T:::::T
+G:::::G     G:::::G          WW         WW                   T:::::T
+G:::::G     G:::::G          WW         WW                   T:::::T
+G:::::G         GGGGGG       WW         WW                   T:::::T
+G:::::GGGGGGGG::::G          WW         WW                   T:::::T
+GG:::::::::::::::G           WW         WW                   T:::::T
+GG:::::::::::::::G           WW         WW                   T:::::T
+ GGGGGGGGGGGGGGG             WW         WW                   TTTTTTT
 "@
 
-# spazio visivo extra tra la sigla e il testo
 Write-Host ""
 Write-Host ""
 Write-Host "====GPUWORLD Tweaks===="
